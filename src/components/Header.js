37eef20354aac24d779432cae11c259663dce675
Daniel Fox <dfox@cfive.com>
2021-06-23 10:53:35 -0600

    Working on filters

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png"
 const Header = () => {
   return <div className="AppNav">
     <img src={logo} data-testid="header-logo" className="App-logo" alt="logo"/>
-    <h1>Galaxy Auto</h1>
+    <h1>Galactic Auto</h1>
   </div>
 }
 
--- src/components/SearchFilters.js
+++ src/components/SearchFilters.js
@@ -3,88 +3,94 @@ import { useState } from "react"
 import Select from 'react-select'
 
 const SearchFilters = () => {
-  const onSelectedOptionsChange = (e) => {
-    switch(e.target.name) {
+  const onSelectedOptionsChange = (selectedArray, action) => {
+    console.log(selectedArray);
+    console.log(action.name);
+    const initialMakeState = {...filterBy.make};
+    const initialModelState = {...filterBy.model};
+    switch(action.name) {
       case "make":
-        console.log(e.target.value)
+        setFilterBy({make: selectedArray, model: initialModelState})
+        // console.log(filterBy)
         break
       case "model":
-        console.log(e.target.value)
-        break
-      case "year-min":
-        console.log(e.target.value)
-        break 
-      case "year-max":
-        console.log(e.target.value)
+        setFilterBy({make: initialMakeState, model: selectedArray})
         break
       default:
         console.log("YOU FOOL")
-        break   
+        break
     }
+    console.log(filterBy)
+  }
+  const sliderChange = (e) => {
+    console.log(e.target.name)
+    console.log(e.target.value)
+    // switch(e.target.name) {
+    //   case ""
+    // }
+    // if (e.target.value > value2) setValue2(e.target.value)
+    // setValue1(e.target.value)}
   }
   const models = [{ value: 'Focus', label: 'Focus' }, { value: "Impala", label: "Impala"}, {value: "Prius", label: "Prius"}]
   const makes = [{ value: 'Ford', label: 'Ford' },{ value: 'Chevrolet', label: 'Chevrolet'}, {value: "Toyota", label: 'Toyota'}, {value: "Volkswagen", label: "Volkswagen" }]
   const [ value1, setValue1 ] = useState(2000);
   const [ value2, setValue2 ] = useState(2050);
-  const [ filterBy, setFilterBy ] = useState({make: [], model: [], year: {min: 0, max: 2050}});
+  const [ filterBy, setFilterBy ] = useState({make: [], model: [], yearMin: 2000, yearMax: 2050});
+
 
   return (
     <>
       <div>
         <h2>Vehicle Filters</h2>
         <Form>
-          <Form.Group>
-            <Form.Label>Make</Form.Label>
-            <Select
-              isMulti
-              name="make"
-              options={makes}
-              className="basic-multi-select"
-              classNamePrefix="select"
-            />
-            {/* <Form.Control name="make" as="select" multiple value={makes} onChange={onSelectedOptionsChange}>
-              {makes.map((option,index) => (
-                <option key={index} value={option}>
-                  {option.name}
-                </option>
-              ))}
-            </Form.Control> */}
-            <br />
-            <Form.Label>Model</Form.Label>
-            <Form.Control name="model" as="select" multiple value={models} onChange={onSelectedOptionsChange}>
-              {models.map((option,index) => (
-                <option key={index} value={option}>
-                  {option.name}
-                </option>
-              ))}
-            </Form.Control>
-            </Form.Group>
-            <br />
-            <Form.Label>Year</Form.Label>
-            <Form.Group as={Row}>
-              <Col xs="6">
-                <span>Min: {value1}</span>
-                <Form.Control name="year-min" type="range"
-                  value={value1}
-                  onChange={e => {
-                    if (e.target.value > value2) setValue2(e.target.value)
-                    setValue1(e.target.value)}}
-                  min={2000}
-                  max={2050}
-                />
-              </Col>
-              <Col xs="6">
-                <span>Max: {value2}</span>
-                <Form.Control name="year-max" type="range"
-                  value={value2}
-                  onChange={e => {
-                    if (e.target.value < value1) setValue1(e.target.value)
-                    setValue2(e.target.value)}}
-                  min={2000}
-                  max={2050}
-                />
-              </Col>
-            </Form.Group>
+          <Form.Group as={Row}>
+            <Col m="6">
+              <Form.Label>Make</Form.Label>
+              <Select
+                isMulti
+                name="make"
+                options={makes}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                onChange={onSelectedOptionsChange}
+              />
+            </Col>
+            <Col m="6">
+              <Form.Label>Model</Form.Label>
+              <Select
+                isMulti
+                name="model"
+                options={models}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                onChange={onSelectedOptionsChange}
+              />
+            </Col>
+          </Form.Group>
+          <br />
+          <Form.Label>Year</Form.Label>
+          <Form.Group as={Row}>
+            <Col xs="6">
+              <span>Min: {value1}</span>
+              <Form.Control name="year-min" type="range"
+                value={value1}
+                onChange={sliderChange}
+                min={2000}
+                max={2050}
+              />
+            </Col>
+            <Col xs="6">
+              <span>Max: {value2}</span>
+              <Form.Control name="year-max" type="range"
+                value={value2}
+                onChange={e => {
+                  if (e.target.value < value1) setValue1(e.target.value)
+                  setValue2(e.target.value)}}
+                min={2000}
+                max={2050}
+              />
+            </Col>
+          </Form.Group>
         </Form>
       </div>
     </>
